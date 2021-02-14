309c59274e3bd5be052a4a73acf27f8976498157
tomek <tomek@fedora.mshome.net>
2021-02-14 09:28:50 +0100

    3nodes

--- generate.sh
+++ generate.sh
@@ -1,4 +1,6 @@
-podman run -i --rm quay.io/coreos/fcct:release --pretty --strict < example.fcc > example.ign
+[tomek@fedora coreos]$ podman run -i --rm quay.io/coreos/fcct:release --pretty --strict < node1.fcc > node1.ign
+[tomek@fedora coreos]$ podman run -i --rm quay.io/coreos/fcct:release --pretty --strict < node2.fcc > node2.ign
+[tomek@fedora coreos]$ podman run -i --rm quay.io/coreos/fcct:release --pretty --strict < node4.fcc > node3.ign
 
 podman run --privileged --pull=always --rm -v .:/data -w /data \
 	    quay.io/coreos/coreos-installer:release download -s stable -p metal -f iso
