8ed2cb4c99f84c340c3dddf4f9056ff9c3a5ddb6
Tomek Grzesiak <tomek.grzesiak@hotmail.com>
2023-02-05 18:54:29 +0100

    wip

--- install.sh
+++ install.sh
@@ -1,4 +1,11 @@
 #/bin/bash
 set -vx
-sudo coreos-installer install /dev/sda \
+sudo coreos-installer install /dev/vda \
 	            --ignition-url https://raw.githubusercontent.com/tomek-grzesiak/coreos/master/${1}.ign
+
+sudo coreos-installer install /dev/vda \
+	            --ignition-url https://raw.githubusercontent.com/tomek-grzesiak/coreos/master/node1.ign
+
+
+sudo coreos-installer install /dev/vda \
+	            --ignition-url https://raw.githubusercontent.com/tomek-grzesiak/coreos/master/node1.ign
