ffd2ad2136a8e47271ca55e818ca2c90f3303b2a
Tomek Grzesiak <tomek.grzesiak@hotmail.com>
2023-02-05 18:30:42 +0100

    wip

--- generate.sh
+++ generate.sh
@@ -1,8 +1,8 @@
 podman run -i --rm quay.io/coreos/fcct:release --pretty --strict < node1.fcc > node1.ign
 podman run -i --rm quay.io/coreos/fcct:release --pretty --strict < node2.fcc > node2.ign
-podman run -i --rm quay.io/coreos/fcct:release --pretty --strict < node4.fcc > node3.ign
+podman run -i --rm quay.io/coreos/fcct:release --pretty --strict < node3.fcc > node3.ign
 
-podman run --privileged --pull=always --rm -v .:/data -w /data \
-	    quay.io/coreos/coreos-installer:release download -s stable -p metal -f iso
+#podman run --privileged --pull=always --rm -v .:/data -w /data \
+#	    quay.io/coreos/coreos-installer:release download -s stable -p metal -f iso
 
-sudp rpm-ostree install python3 pip  python3-libselinux
+#sudp rpm-ostree install python3 pip  python3-libselinux
